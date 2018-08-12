da0d57584a614cc0313e35a8de25bbe6863cad33
lancerXXXX <2571021108@qq.com>
2018-08-12 12:11:16 +0800

    加入创建数据库和表的各种配置。

---
+++ app/src/main/java/com/lancer/dell/coolweather/db/Province.java
@@ -0,0 +1,33 @@
+package com.lancer.dell.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id ;
+    private String proviceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProviceName() {
+        return proviceName;
+    }
+
+    public void setProviceName(String proviceName) {
+        this.proviceName = proviceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
